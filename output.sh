50b9bceca80290960dc2594b16894d1d9115f747
宋岩叡 <b06902106@ntu.edu.tw>
2020-05-01 22:23:29 +0800

    upload report pdf

--- output.sh
+++ output.sh
@@ -4,7 +4,7 @@ num="1 2 3 4 5"
 for task in FIFO PSJF RR SJF; do
     for num in 1 2 3 4 5; do
         echo "${task}_${num}.txt"
-        sudo ./scheduler < test/${task}_${num}.txt > output/${task}_${num}_out.txt
+        sudo ./scheduler < test/${task}_${num}.txt > output/${task}_${num}_stdout.txt
         sudo dmesg | grep Project1 > output/${task}_${num}_dmesg.txt
         sudo dmesg -C
     done
